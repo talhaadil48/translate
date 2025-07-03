1dab4ead4cb7ee365284f548201bedc9159bc931
talhaadil48 <talhaadil48@icloud.com>
2025-07-03 16:26:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://sus-forest.vercel.app"
-    ],
+    allow_origins=["*"],  # <-- Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
